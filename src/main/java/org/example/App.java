0b2d113b7ece13616169dc54ddde23741b1f5d43
Sergey <derbin.s-a@yandex.ru>
2020-09-13 12:26:46 +0300

    Correct and refactoring code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,33 +4,41 @@ import static java.util.concurrent.ForkJoinTask.invokeAll;
 
 public class App {
     public static void main(String[] args) {
+        long[] array = generateMas();
+
         long begin = System.currentTimeMillis();
         double avg = 0;
-        long[] array = generateMas();
 
         // ОДНИМ ПОТОКОМ
         for (double value : array) {
             avg += value;
         }
-        System.out.println("Среднее арифметическое однопоточно: " + avg / array.length);
+        avg = avg / array.length;
+        System.out.println("Среднее арифметическое однопоточно: " + avg);
         long end = System.currentTimeMillis();
         System.out.println("Время выполнения однопоточно:" + (end - begin) + "мс");
 
         // ForkJoinPool
         begin = System.currentTimeMillis();
-        final int middle = (array.length) / 2;
-        ArraySumTask task1 = new ArraySumTask(array, 0, middle);
-        ArraySumTask task2 = new ArraySumTask(array, middle, array.length);
-        invokeAll(task1, task2);
-
-        System.out.println("Среднее арифметическое многопоточно: " + (task1.join() + task2.join()));
+        double sum = sumArray(array);
         end = System.currentTimeMillis();
+
+        System.out.println("Среднее арифметическое многопоточно: " + sum/array.length);
         System.out.println("Время выполнения многопоточно:" + (end - begin) + "мс");
 
     }
 
+    private static int sumArray(long[] array) {
+        final int middle = (array.length) / 2;
+        ArraySumTask task1 = new ArraySumTask(array, 0, middle);
+        ArraySumTask task2 = new ArraySumTask(array, middle, array.length);
+
+        invokeAll(task1,task2);
+        return task1.join()+task2.join();
+    }
+
     public static long[] generateMas() {
-        long[] array = new long[1000000];
+        long[] array = new long[10000000];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) Math.round((Math.random() * 100) - 0);
         }
--- src/main/java/org/example/ArraySumTask.java
+++ src/main/java/org/example/ArraySumTask.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.concurrent.RecursiveTask;
 
-public class ArraySumTask extends RecursiveTask<Double> {
+public class ArraySumTask extends RecursiveTask<Integer> {
     private final long[] mas;
     private final int start;
     private final int end;
@@ -14,14 +14,17 @@ public class ArraySumTask extends RecursiveTask<Double> {
     }
 
     @Override
-    protected Double compute() {
-        double sum = 0;
-        double avg;
-        long length = end - start;
+    protected Integer compute() {
+        int sum = 0;
+        long length;
+        if (start == mas.length / 2) {
+            length = mas.length;
+        } else {
+            length = end - start;
+        }
         for (int x = start; x < length; x++) {
             sum += mas[x];
         }
-        avg = sum / length;
-        return avg;
+        return sum;
     }
 }
